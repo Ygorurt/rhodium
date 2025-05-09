cb5f019a89ad41b3141658bb51ab835bd9034b6f
Ygorurt <54077930+Ygorurt@users.noreply.github.com>
2025-05-09 18:40:52 -0300

    Update mainwindow.h

--- backend/src/gui/mainwindow.h
+++ backend/src/gui/mainwindow.h
@@ -2,16 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QPushButton>
-#include <QTextEdit>
-#include <QLabel>
-#include "../blockchain/blockchain.h"
+#include "blockchain.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
 public:
@@ -19,16 +17,12 @@ public:
     ~MainWindow();
 
 private slots:
-    void onMineButtonClicked();
-    void onSendPixClicked();
     void updateChainStatus();
+    void onMineButtonClicked();
 
 private:
     Ui::MainWindow *ui;
-    Blockchain blockchain_;
-    QTimer *updateTimer_;
-    
-    void setupUI();
-    void log(const QString& message);
+    Blockchain blockchain;
+    QTimer *updateTimer;
 };
 #endif // MAINWINDOW_H
