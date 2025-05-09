edb5ac66ef8b3cebdc33e978696a3ccd4da8a276
Ygorurt <54077930+Ygorurt@users.noreply.github.com>
2025-05-09 18:41:45 -0300

    Update mainwindow.cpp

--- backend/src/gui/mainwindow.cpp
+++ backend/src/gui/mainwindow.cpp
@@ -1,35 +1,46 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QTimer>
+#include <QString>
 
-ainWindow::MainWindow(QWidget *parent)
+MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , blockchain()
+    , updateTimer(new QTimer(this))
 {
     ui->setupUi(this);
-    
-    // Corrigido - removido .size() e adicionado cast para int
+
+    // Configuração inicial dos labels
     ui->chainLengthLabel->setText(QString("Comprimento da cadeia: %1")
-                                .arg(static_cast<int>(blockchain.getChainLength())));
-    
+                                .arg(blockchain.getChainLength()));
     ui->pendingTxsLabel->setText(QString("Transações pendentes: %1")
-                               .arg(static_cast<int>(blockchain.getPendingTransactions())));
-    
-    updateTimer_ = new QTimer(this);
-    connect(updateTimer_, &QTimer::timeout, this, &MainWindow::updateChainStatus);
-    updateTimer_->start(5000); // Update every 5 seconds
+                               .arg(blockchain.getPendingTransactions()));
+
+    // Configuração do timer
+    connect(updateTimer, &QTimer::timeout, this, &MainWindow::updateChainStatus);
+    updateTimer->start(5000); // Atualiza a cada 5 segundos
+
+    // Conexão dos botões
+    connect(ui->mineButton, &QPushButton::clicked, 
+            this, &MainWindow::onMineButtonClicked);
 }
 
-void MainWindow::onMineButtonClicked() {
-    if (blockchain_.mineBlock("miner_address")) {
-        log("New block mined successfully!");
-    } else {
-        log("No transactions to mine");
-    }
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::updateChainStatus()
+{
+    ui->chainLengthLabel->setText(QString("Comprimento da cadeia: %1")
+                                .arg(blockchain.getChainLength()));
+    ui->pendingTxsLabel->setText(QString("Transações pendentes: %1")
+                               .arg(blockchain.getPendingTransactions()));
+}
+
+void MainWindow::onMineButtonClicked()
+{
+    blockchain.mineBlock("miner_address");
     updateChainStatus();
-}
-
-void MainWindow::updateChainStatus() {
-    ui->chainLengthLabel->setText(QString("Blockchain length: %1").arg(blockchain_.getChainLength()));
-    ui->pendingTxsLabel->setText(QString("Pending transactions: %1").arg(blockchain_.getPendingTransactions().size()));
 }
