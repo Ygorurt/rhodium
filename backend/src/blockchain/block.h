1cc3323a2c9a6c74e88bd36f8581e11fb25ac7bd
Ygorurt <54077930+Ygorurt@users.noreply.github.com>
2025-05-11 00:19:49 -0300

    Update block.h

--- backend/src/blockchain/block.h
+++ backend/src/blockchain/block.h
@@ -4,23 +4,24 @@
 #include <string>
 #include <vector>
 #include <ctime>
-#include <nlohmann/json.hpp>  // Adicione esta linha
 #include "transaction.h"
+#include <nlohmann/json.hpp>
 
 class Block {
 public:
-    Block(int index, const std::string& previousHash, const std::vector<Transaction>& transactions);
+    Block(int index, const std::string& previousHash, 
+          const std::vector<Transaction>& transactions);
     
     std::string getHash() const;
     void incrementNonce();
-    nlohmann::json toJson() const;  // Corrigido
+    nlohmann::json toJson() const;
     
 private:
     int index_;
     std::time_t timestamp_;
     std::string previousHash_;
     std::vector<Transaction> transactions_;
-    int nonce_ = 0;
+    int nonce_;
     std::string hash_;
     
     std::string calculateHash() const;
