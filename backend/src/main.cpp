4dcd876fba42c7c320922e1c7bfc19d170f95aca
Ygorurt <54077930+Ygorurt@users.noreply.github.com>
2025-05-11 00:22:06 -0300

    Update main.cpp

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,7 +1,8 @@
 #include "gui/mainwindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
