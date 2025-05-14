965a00a773e520115105c0ad108c680ab7e5eebc
Ygorurt <54077930+Ygorurt@users.noreply.github.com>
2025-05-14 20:39:55 -0300

    Update mainwindow.cpp

--- backend/src/gui/mainwindow.cpp
+++ backend/src/gui/mainwindow.cpp
@@ -1,141 +1,67 @@
 #include "mainwindow.h"
+#include <QVBoxLayout>
 #include <QMenuBar>
-#include <QMessageBox>
-#include <QTimer>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{
-    setWindowTitle("Rhodium Blockchain Node");
-    resize(1024, 768);
+    : QMainWindow(parent), blockchain() {
+    
+    // Initialize P2P network
+    blockchain.initP2PNetwork();
+    
+    setupUI();
+    setupConnections();
+}
 
-    // Inicializa blockchain
-    blockchain.initP2PNetwork(8333);
+void MainWindow::setupUI() {
+    // Main tabs
+    mainTabs = new QTabWidget(this);
+    setCentralWidget(mainTabs);
+    
+    // Create widgets
+    txWidget = new TransactionWidget(&blockchain);
+    powMiner = new POWMinerWidget(&blockchain);
+    posValidator = new POSValidatorWidget(&blockchain);
+    networkWidget = new NetworkWidget(&blockchain);
+    
+    // Add tabs
+    mainTabs->addTab(txWidget, "Transações");
+    mainTabs->addTab(powMiner, "Mineração PoW");
+    mainTabs->addTab(posValidator, "Validação PoS");
+    mainTabs->addTab(networkWidget, "Rede P2P");
+    
+    // Status bar
+    chainStatus = new QLabel("Blockchain: Carregando...");
+    peerStatus = new QLabel("Peers: 0");
+    miningStatus = new QLabel("Mineração: Inativa");
+    
+    statusBar()->addWidget(chainStatus);
+    statusBar()->addWidget(peerStatus);
+    statusBar()->addWidget(miningStatus);
+    
+    setWindowTitle("Node de Blockchain Avançado");
+    resize(800, 600);
+}
 
-    setupUi();
-    setupMenu();
-    setupTrayIcon();
-    connectSignals();
-
-    // Atualizador de status
+void MainWindow::setupConnections() {
+    connect(&blockchain, &Blockchain::blockAdded, this, &MainWindow::onBlockMined);
+    connect(&blockchain, &Blockchain::transactionAdded, this, &MainWindow::onTransactionAdded);
+    
+    // Update status every 5 seconds
     QTimer *statusTimer = new QTimer(this);
-    connect(statusTimer, &QTimer::timeout, this, &MainWindow::updateStatus);
+    connect(statusTimer, &QTimer::timeout, this, &MainWindow::updateStatusBar);
     statusTimer->start(5000);
 }
 
-void MainWindow::setupUi()
-{
-    // Widget principal
-    tabWidget = new QTabWidget(this);
-    setCentralWidget(tabWidget);
-
-    // Cria as abas
-    walletWidget = new WalletWidget(blockchain, this);
-    networkWidget = new NetworkWidget(blockchain, this);
-    miningWidget = new MiningWidget(blockchain, this);
-    transactionWidget = new TransactionWidget(blockchain, this);
-
-    tabWidget->addTab(walletWidget, "Carteira");
-    tabWidget->addTab(networkWidget, "Rede");
-    tabWidget->addTab(miningWidget, "Mineração");
-    tabWidget->addTab(transactionWidget, "Transações");
-
-    // Barra de status
-    statusBar = new QStatusBar(this);
-    setStatusBar(statusBar);
-    updateStatus();
+void MainWindow::updateStatusBar() {
+    chainStatus->setText(QString("Blocos: %1").arg(blockchain.getChainLength()));
+    peerStatus->setText(QString("Peers: %1").arg(blockchain.getPeerList().size()));
 }
 
-void MainWindow::setupMenu()
-{
-    QMenu *fileMenu = menuBar()->addMenu("Arquivo");
-    fileMenu->addAction("Salvar Blockchain", [this]() {
-        blockchain.saveToFile("blockchain.json");
-        showNotification("Blockchain Salva", "Blockchain salva com sucesso no arquivo blockchain.json");
-    });
-    fileMenu->addSeparator();
-    fileMenu->addAction("Sair", qApp, &QApplication::quit);
-
-    QMenu *helpMenu = menuBar()->addMenu("Ajuda");
-    helpMenu->addAction("Sobre", []() {
-        QMessageBox::about(nullptr, "Sobre", "Rhodium Blockchain\nVersão 1.0");
-    });
+void MainWindow::onBlockMined(const Block& newBlock) {
+    statusBar()->showMessage("Novo bloco minerado: " + QString::fromStdString(newBlock.hash), 3000);
+    updateStatusBar();
 }
 
-void MainWindow::setupTrayIcon()
-{
-    trayIcon = new QSystemTrayIcon(QIcon(":/icons/rhodium.png"), this);
-    
-    QMenu *trayMenu = new QMenu(this);
-    trayMenu->addAction("Abrir", this, &QWidget::showNormal);
-    trayMenu->addAction("Sair", qApp, &QApplication::quit);
-    
-    trayIcon->setContextMenu(trayMenu);
-    trayIcon->show();
-    
-    connect(trayIcon, &QSystemTrayIcon::activated, [this](QSystemTrayIcon::ActivationReason reason) {
-        if (reason == QSystemTrayIcon::DoubleClick) {
-            showNormal();
-        }
-    });
-}
-
-void MainWindow::connectSignals()
-{
-    // Conecta sinais da blockchain à interface
-    connect(&blockchain, &Blockchain::blockMined, this, &MainWindow::onBlockMined);
-    connect(&blockchain, &Blockchain::transactionAdded, this, &MainWindow::onTransactionProcessed);
-    connect(&blockchain, &Blockchain::peerConnected, this, &MainWindow::onPeerConnected);
-    connect(&blockchain, &Blockchain::peerDisconnected, this, &MainWindow::onPeerDisconnected);
-}
-
-void MainWindow::updateStatus()
-{
-    QString status = QString("Blocos: %1 | Transações Pendentes: %2 | Conexões: %3")
-                    .arg(blockchain.getChainLength())
-                    .arg(blockchain.getPendingTransactions())
-                    .arg(blockchain.getPeerList().size());
-    
-    statusBar->showMessage(status);
-}
-
-void MainWindow::showNotification(const QString &title, const QString &message)
-{
-    if (isMinimized() || !isActiveWindow()) {
-        trayIcon->showMessage(title, message);
-    } else {
-        statusBar->showMessage(message, 5000);
-    }
-}
-
-void MainWindow::onBlockMined(const QString &blockHash)
-{
-    showNotification("Novo Bloco Minerado", QString("Bloco %1 adicionado à blockchain").arg(blockHash));
-    walletWidget->refreshBalance();
-    miningWidget->updateStatus();
-}
-
-void MainWindow::onTransactionProcessed(const QString &txId)
-{
-    showNotification("Nova Transação", QString("Transação %1 processada").arg(txId));
-    transactionWidget->updateTransactions();
-    walletWidget->refreshBalance();
-}
-
-void MainWindow::onPeerConnected(const QString &peer)
-{
-    showNotification("Nova Conexão", QString("Conectado ao nó %1").arg(peer));
-    networkWidget->updatePeerList();
-}
-
-void MainWindow::onPeerDisconnected(const QString &peer)
-{
-    showNotification("Conexão Encerrada", QString("Desconectado do nó %1").arg(peer));
-    networkWidget->updatePeerList();
-}
-
-MainWindow::~MainWindow()
-{
-    // Salva blockchain ao sair
-    blockchain.saveToFile("blockchain_autosave.json");
+void MainWindow::onTransactionAdded(const Transaction& tx) {
+    statusBar()->showMessage("Nova transação adicionada: " + QString::fromStdString(tx.txHash), 3000);
 }
