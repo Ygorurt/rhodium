8d1dad6596237a6017e6d7c2e4cdec47793667d6
Ygorurt <54077930+Ygorurt@users.noreply.github.com>
2025-05-14 20:25:57 -0300

    Update peer.h

--- backend/src/network/peer.h
+++ backend/src/network/peer.h
@@ -1,47 +1,26 @@
 #ifndef PEER_H
 #define PEER_H
 
+#include <QObject>
 #include <QTcpSocket>
-#include <QHostAddress>
-#include <QDateTime>
-#include <QString>
+#include "message.h"  // Inclua o arquivo de cabeçalho da Message
 
-struct PeerInfo {
-    QHostAddress address;
-    quint16 port;
-    QString publicKey;
-    QString version;
-    qint64 lastSeen;
-    int latency; // em ms
-    QString id;  // Identificador único
-
-    bool isValid() const;
-    QString toString() const;
-};
-
-class Peer : public QTcpSocket {
+class Peer : public QObject {
     Q_OBJECT
-
 public:
-    explicit Peer(QObject* parent = nullptr);
-    explicit Peer(const PeerInfo& info, QObject* parent = nullptr);
-
-    PeerInfo info() const;
-    void setInfo(const PeerInfo& info);
+    explicit Peer(QObject *parent = nullptr);
+    
+    // Corrija as declarações adicionando os tipos de retorno
+    void sendMessage(const Message &message);
     bool isConnected() const;
-    void sendMessage(const Message& message);
 
 signals:
-    void messageReceived(const Message& message);
-    void disconnected(Peer* peer);
-
-private slots:
-    void onReadyRead();
-    void onDisconnected();
+    // Especifique o tipo completo para o parâmetro
+    void messageReceived(const Message &message);
+    void disconnected();
 
 private:
-    PeerInfo m_info;
-    QByteArray m_buffer;
+    QTcpSocket *socket;
 };
 
 #endif // PEER_H
