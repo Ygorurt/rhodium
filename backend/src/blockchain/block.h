ebb31b1d06df3a665af21f6295a71a12ee8cf1b8
Ygorurt <54077930+Ygorurt@users.noreply.github.com>
2025-05-14 20:49:26 -0300

    Update block.h

--- backend/src/blockchain/block.h
+++ backend/src/blockchain/block.h
@@ -1,38 +1,20 @@
-#ifndef BLOCK_H
-#define BLOCK_H
-
-#include <string>
+// block.h
+#pragma once
 #include <vector>
-#include <ctime>
-#include <nlohmann/json.hpp>
-#include "crypto/sha256.h"
-
-class Transaction;
+#include <string>
+#include "transaction.h"
 
 class Block {
 public:
-    Block(size_t index, const std::string& previousHash, const std::vector<Transaction>& transactions);
+    Block() = default;
+    Block(size_t height, time_t timestamp, std::string previousHash, 
+          std::string miner, std::vector<Transaction> transactions);
     
-    size_t getIndex() const;
-    std::string getPreviousHash() const;
-    std::string getHash() const;
-    std::string getMinerAddress() const;
-    const std::vector<Transaction>& getTransactions() const;
-    
-    void incrementNonce();
-    nlohmann::json toJson() const;
-    std::string serialize() const;
-    static Block deserialize(const std::string& data);
-    
-private:
-    size_t index_;
-    std::string previousHash_;
-    std::string hash_;
-    std::vector<Transaction> transactions_;
-    int nonce_;
-    time_t timestamp_;
-    std::string minerAddress_;
-    
-    std::string calculateHash() const;
+    size_t height;
+    time_t timestamp;
+    std::string previousHash;
+    std::string hash;
+    std::string miner;
+    uint64_t nonce;
+    std::vector<Transaction> transactions;
 };
-#endif // BLOCK_H
