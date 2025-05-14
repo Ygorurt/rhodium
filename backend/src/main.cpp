30a54cafb1725095466b76dc8d3d400d88cb764e
Ygorurt <54077930+Ygorurt@users.noreply.github.com>
2025-05-13 22:52:32 -0300

    Update main.cpp

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,8 +1,13 @@
 #include "gui/mainwindow.h"
 #include <QApplication>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
+    // Configura o caminho dos plugins
+    QCoreApplication::addLibraryPath("./platforms");
+    qputenv("QT_QPA_PLATFORM_PLUGIN_PATH", "platforms");
+    
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
