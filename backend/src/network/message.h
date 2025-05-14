2c9cf0d912fc95b1fd7671b956095af15c7dd5eb
Ygorurt <54077930+Ygorurt@users.noreply.github.com>
2025-05-14 20:26:44 -0300

    Update message.h

--- backend/src/network/message.h
+++ backend/src/network/message.h
@@ -2,53 +2,28 @@
 #define MESSAGE_H
 
 #include <QByteArray>
-#include <QDateTime>
-#include <QList>
-#include "network/peer.h"
+#include <QDataStream>
 
 class Message {
 public:
     enum Type {
-        TRANSACTION = 0x01,
-        BLOCK = 0x02,
-        GET_PEERS = 0x03,
-        PEERS = 0x04,
-        PING = 0x05,
-        PONG = 0x06,
-        HANDSHAKE = 0x07
+        TRANSACTION,
+        BLOCK,
+        PEER_LIST
     };
 
-    static const int MAX_MESSAGE_SIZE = 10 * 1024 * 1024; // 10MB
+    static const int MIN_MESSAGE_SIZE = 10;  // Defina o tamanho mínimo
 
-    Message(Type type, const QByteArray& payload);
-    Message(const QByteArray& rawData);
-
-    bool isValid() const;
+    Message(Type type, const QByteArray &data);
+    
     Type type() const;
-    QByteArray payload() const;
-    QByteArray rawData() const;
-    QDateTime timestamp() const;
-
-    // Factory methods
-    static Message createHandshake(const PeerInfo& peerInfo);
-    static Message createTransaction(const QByteArray& txData);
-    static Message createBlock(const QByteArray& blockData);
-    static Message createGetPeers();
-    static Message createPeers(const QList<PeerInfo>& peers);
-    static Message createPing();
-    static Message createPong();
+    QByteArray data() const;
+    QByteArray serialize() const;
+    static Message deserialize(const QByteArray &data);
 
 private:
     Type m_type;
-    QByteArray m_payload;
-    QDateTime m_timestamp;
-    bool m_valid;
-
-    QByteArray serialize() const;
-    void deserialize(const QByteArray& data);
-
-    friend QDataStream& operator<<(QDataStream&, const PeerInfo&);
-    friend QDataStream& operator>>(QDataStream&, PeerInfo&);
+    QByteArray m_data;
 };
 
 #endif // MESSAGE_H
