b2d0b2c50bd26658fab1dd722d462120b0698c55
Ygorurt <54077930+Ygorurt@users.noreply.github.com>
2025-05-14 20:41:53 -0300

    Update blockchain.h

--- backend/src/blockchain/blockchain.h
+++ backend/src/blockchain/blockchain.h
@@ -1,7 +1,6 @@
 #ifndef BLOCKCHAIN_H
 #define BLOCKCHAIN_H
 
-#pragma once
 #include <vector>
 #include <string>
 #include <mutex>
@@ -9,38 +8,46 @@
 #include <nlohmann/json.hpp>
 #include "block.h"
 #include "transaction.h"
-#include "network/p2p.h"  // Nova inclusão
+#include "network/p2p.h"
 
 class Blockchain {
 public:
     Blockchain();
     
-    // Métodos existentes
+    // Métodos básicos
     bool mineBlock(const std::string& minerAddress, bool usePoS = false);
     void saveToFile(const std::string& filename) const;
     std::string lastHash() const;
-    void addTransaction(const std::string& from, const std::string& to, double amount);
+    void addTransaction(const std::string& from, const std::string& to, double amount, const std::string& signature = "");
     double getBalance(const std::string& address) const;
     size_t getChainLength() const;
     size_t getPendingTransactions() const;
     nlohmann::json toJson() const;
     
-    // Novos métodos P2P
+    // Métodos PoS
+    double getStake(const std::string& address) const;
+    std::vector<std::string> getValidators() const;
+    
+    // Rede P2P
     void initP2PNetwork(int port = 8333);
     void broadcastTransaction(const Transaction& tx);
     void broadcastBlock(const Block& block);
     std::vector<std::string> getPeerList() const;
-    
+
+signals:
+    void blockAdded(const Block& newBlock);
+    void transactionAdded(const Transaction& tx);
+    void peersUpdated();
+
 private:
     std::vector<Block> chain_;
     std::vector<Transaction> pendingTxs_;
     std::unordered_map<std::string, double> balances_;
+    std::unordered_map<std::string, double> stakes_;
     mutable std::mutex mutex_;
     
-    // Rede P2P
     std::unique_ptr<P2PNetwork> p2pNetwork_;
     
-    // Novos métodos internos
     void processReceivedTransaction(const Transaction& tx);
     void processReceivedBlock(const Block& block);
     void syncChain();
