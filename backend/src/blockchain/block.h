1a0112f7ae7a923e46a877dbf4f24d131d1913c1
Ygorurt <54077930+Ygorurt@users.noreply.github.com>
2025-05-14 00:45:06 -0300

    Update block.h

--- backend/src/blockchain/block.h
+++ backend/src/blockchain/block.h
@@ -6,25 +6,32 @@
 #include <ctime>
 #include "transaction.h"
 #include <nlohmann/json.hpp>
+#include "crypto/sha256.h"
 
 class Block {
 public:
-    Block(int index, const std::string& previousHash, 
-          const std::vector<Transaction>& transactions);
+    Block(size_t index, const std::string& previousHash, const std::vector<Transaction>& transactions);
     
+    size_t getIndex() const;
+    std::string getPreviousHash() const;
     std::string getHash() const;
+    std::string getMinerAddress() const;
+    const std::vector<Transaction>& getTransactions() const;
+    
     void incrementNonce();
     nlohmann::json toJson() const;
+    std::string serialize() const;
+    static Block deserialize(const std::string& data);
     
 private:
-    int index_;
-    std::time_t timestamp_;
+    size_t index_;
     std::string previousHash_;
+    std::string hash_;
     std::vector<Transaction> transactions_;
     int nonce_;
-    std::string hash_;
+    time_t timestamp_;
+    std::string minerAddress_;
     
     std::string calculateHash() const;
 };
-
 #endif // BLOCK_H
